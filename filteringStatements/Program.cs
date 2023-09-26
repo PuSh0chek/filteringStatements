462024474184424c61b4f6cd8a94f04f754c16d9
Alex <87208475+PuSh0chek@users.noreply.github.com>
2023-09-26 16:04:03 +0300

    Добавьте файлы проекта.

---
+++ filteringStatements/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace FilterStatements;
+using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        string objectForFilter = "Олейников Руслан Витальевич\n№153-В от 2017г. о подключ. к с-ме водоснабж. ул.37 Линия, 95\nПоступление наличных 00000001264 от 16.03.2018 18:57:28";
+        string contractFirstOption = @"d{1,4}\s*-{0,2}\s*[в,к,В,К]{1,2}\s*от\s*\d{1,2}.\d{1,2}.\d{4})";
+
+
+    }
+}
