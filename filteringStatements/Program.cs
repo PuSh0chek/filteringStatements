d15b911fb725c70177fabc4dc3381556ed2866f6
Alex <87208475+PuSh0chek@users.noreply.github.com>
2023-09-26 16:27:55 +0300

    0.0.0

--- filteringStatements/Program.cs
+++ filteringStatements/Program.cs
@@ -7,9 +7,11 @@ class Program
 {
     static void Main()
     {
-        string objectForFilter = "Олейников Руслан Витальевич\n№153-В от 2017г. о подключ. к с-ме водоснабж. ул.37 Линия, 95\nПоступление наличных 00000001264 от 16.03.2018 18:57:28";
-        string contractFirstOption = @"d{1,4}\s*-{0,2}\s*[в,к,В,К]{1,2}\s*от\s*\d{1,2}.\d{1,2}.\d{4})";
+        string objectForFilter = "02.11.2022, 166-К,  20.06.22, 62.02, 150783, 51, -150783, Южный филиал ПАО \"Промсвязьбанк\" (Расчетный) Основной 700\nПоступления - выполнение действий по подготовке ВиК к подключению, РЖД (ОАО)\n№166-К от 20.06.22 о подключ. к с-ме водоотвед. пр-кт Сельмаш, 1а\nПоступление на расчетный счет 00000003723 от 30.06.2022 23:59:59";
+        string contractFirstOption = @"\s\d{2}.\d{2}.\d{2,4}";
+        Regex regex = new Regex(objectForFilter);
 
-
+        MatchCollection match = regex.Matches(contractFirstOption);
+        Console.WriteLine(match.Count);
     }
 }
